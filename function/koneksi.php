e6380312f866c01b58c7a3fdd5e41ca1a37e7257
devarahifz <devara.hifzhurrahman22@gmail.com>
2020-05-06 21:12:59 +0700

    Module Kategori

--- function/koneksi.php
+++ function/koneksi.php
@@ -2,7 +2,7 @@
 
     $server = "localhost";
     $username = "root";
-    $password = "";
+    $password = "gooners182";
     $database = "database_toko";
 
     $koneksi = mysqli_connect($server, $username, $password, $database) or die("Koneksi ke database gagal.");
